6613a7cd85d25e0e9c1ac311a2326f234e94dc09
gautham <desertncamel@gmail.com>
2024-10-11 07:38:18 +0530

    nodemon and boiler for cloudStorage routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const cloudRoutes = require('./routes/cloudStorage');
+
+app.use('/cloud', cloudRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
