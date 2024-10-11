a4f7d25a8523d2ade903ff4e760b96a7df519068
gautham <desertncamel@gmail.com>
2024-10-11 15:49:39 +0530

    basics excluding bucket details

--- routes/cloudStorage.js
+++ routes/cloudStorage.js
@@ -1,7 +1,29 @@
+require('dotenv').config();
+const {Storage} = require('@google-cloud/storage');
 const express = require('express');
 
-
+// Initializations and env variables
+const storage = new Storage();
 const router = express.Router();
+const projectID = process.env.PROJECT_ID;
+const bucketName = process.env.BUCKET_NAME;
+const keyFileName = process.env.KEYFILENAME;
+
+async function uploadFile(bucketN,file,fileOutputName) {
+    try{
+        const storage = new Storage({projectID, keyFileName});
+        const bucket = storage.bucket(bucketN);
+        const ret = await bucket.upload(file, {
+            destination: fileOutputName
+        });
+        console.log(ret);
+    }catch(error){
+        console.err(error);
+    }
+}
+(async ()=>{
+    uploadFile(bucketName,'TEST.TXT')
+})
 
 router.get('/', (req, res) => {
     res.send("hello world");
